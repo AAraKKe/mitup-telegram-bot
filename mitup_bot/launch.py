4d6a7dc246a5312209d4604e6935e7e233d401d7
Juanpe Araque <erjuanpea@gmail.com>
2023-09-28 19:19:05 +0000

    Add CI/CD support and a Dockerfile to be used as CI image

--- mitup_bot/launch.py
+++ mitup_bot/launch.py
@@ -1,20 +1,23 @@
 import logging
+
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
-        chat_id=update.effective_chat.id, text="Hello, world")
+        chat_id=update.effective_chat.id, text="Hello, world"
+    )
 
-if __name__ == '__main__':
-    application = ApplicationBuilder().token('xxxxxx').build()
-    
-    start_handler = CommandHandler('start', start)
+
+if __name__ == "__main__":
+    application = ApplicationBuilder().token("xxxxxx").build()
+
+    start_handler = CommandHandler("start", start)
     application.add_handler(start_handler)
-    
+
     application.run_polling()
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,6 +1,19 @@
-class TestClass:
-    def test_one(self):
-        assert (2**4 == 16)
+from unittest import mock
 
-    def test_two(self):
-        assert (4 + 4 == 8)
+import pytest
+
+from mitup_bot.launch import start
+
+
+# This is a dummy test that actually tests some code to produce
+# valid data for coverage report.
+@pytest.mark.asyncio
+async def test_dummy():
+    mock_update = mock.Mock()
+    mock_update.update.effective_chat.id = 12
+    mock_context = mock.Mock()
+    mock_context.bot.send_message = mock.AsyncMock()
+
+    await start(mock_update, mock_context)
+
+    mock_context.bot.send_message.assert_called_once()
