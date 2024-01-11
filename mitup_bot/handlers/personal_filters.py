75f8dcb73174d2ad935eef147e007905fc6d3092
Enrique Araque <minixute@gmail.com>
2024-01-11 18:34:48 +0100

    Add user exist filter

--- mitup_bot/handlers/personal_filters.py
+++ mitup_bot/handlers/personal_filters.py
@@ -1,10 +1,13 @@
-from telegram import Message
-from telegram.ext.filters import MessageFilter
+from telegram import Update
+from telegram.ext.filters import UpdateFilter
 from mitup_bot.models import User
 
 
-class UserExistFilter(MessageFilter):
-    def filter(self, message: Message) -> bool:
-        user = message.from_user.id
-        with User.open_session():
-            return User.find_by_tg_user_id(user) is not None
+class UserExistFilter(UpdateFilter):
+    def filter(self, update: Update) -> bool:
+        if update.effective_user is not None:
+            user = update.effective_user.id
+            with User.open_session():
+                return User.find_by_tg_user_id(user) is not None
+
+        return False
