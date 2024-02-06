9fc789b276b3c944b299f058e08015a440f91269
Enrique Araque <minixute@gmail.com>
2024-02-06 12:02:56 +0100

    Add callback query handlers in the init file

--- mitup_bot/handlers/__init__.py
+++ mitup_bot/handlers/__init__.py
@@ -3,10 +3,12 @@ __all__ = (
     "commands",
     "HandlersRegistry",
     "messages",
+    "callback_query",
     "conversations",
 )
 
 from . import commands
 from . import messages
+from . import callback_query
 from . import conversations
 from .registry import HandlersRegistry
