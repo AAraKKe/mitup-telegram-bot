1f62eccb658cf4f9314ce4ea1d234fa4c8a8780e
Enrique Araque <minixute@gmail.com>
2024-02-06 11:26:31 +0100

    Add api for view and send messages

---
+++ mitup_bot/api.py
@@ -0,0 +1,39 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+
+from mitup_bot.views import MitupView
+
+
+async def send_message_view(
+    context: ContextTypes.DEFAULT_TYPE,
+    update: Update,
+    view: MitupView,
+):
+    if update.effective_chat is None:
+        raise RuntimeError("Effective chat not set")
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=view.description,
+        reply_markup=view.markup,
+        parse_mode='MarkdownV2'
+    )
+
+
+async def edit_message_view(
+    context: ContextTypes.DEFAULT_TYPE,
+    update: Update,
+    view: MitupView,
+):
+    if update.effective_chat is None:
+        raise RuntimeError("Effective chat not set")
+
+    if update.effective_message is not None:
+        message_id = update.effective_message.message_id
+
+        await context.bot.edit_message_text(view.description,
+                                            update.effective_chat.id,
+                                            message_id=message_id,
+                                            reply_markup=view.markup,
+                                            parse_mode='MarkdownV2'
+                                            )
