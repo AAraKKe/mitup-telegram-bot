00790d0e445c24f9a6b3a3bd88438997731da5f3
Enrique Araque <minixute@gmail.com>
2024-02-09 21:04:23 +0100

    Fix settings update

--- mitup_bot/handlers/messages.py
+++ mitup_bot/handlers/messages.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ContextTypes, filters, ConversationHandler
 
 from .registry import HandlersRegistry
-from mitup_bot.models import User
+from mitup_bot.models import User, Settings
 from mitup_bot.views.views import settings_view
 from mitup_bot.api import send_message, send_message_view
 
@@ -31,16 +31,33 @@ async def second_timezone_message_handler(update: Update, context: ContextTypes.
     if update.effective_chat is None:
         raise RuntimeError("Effective chat not set")
 
+    # with User.open_session():
+    #     if update.effective_user is not None and update.effective_message is not None:
+    #         if user := User.find_by_tg_user_id(update.effective_user.id):
+    #             if update.effective_message.text is not None:
+    #                 user.settings.timezone = update.effective_message.text
+    #                 user.update()
+
+    #                 message = f"Your timezone has been set to: *{user.settings.timezone}* "
+    #                 view = settings_view(message)
+
+    #                 await send_message_view(context, update, view)
+
     with User.open_session():
-        if update.effective_user is not None and update.effective_message is not None:
-            if user := User.find_by_tg_user_id(update.effective_user.id):
-                if update.effective_message.text is not None:
-                    user.settings.timezone = update.effective_message.text
-                    user.update()
+        user_settings = None
+        if update.effective_user is None:
+            raise RuntimeError("Effective user not set")
 
-                    message = f"Your timezone has been set to: *{user.settings.timezone}* "
-                    view = settings_view(message)
+        user_settings = User.get_settings_from_user(update.effective_user.id)
 
-                    await send_message_view(context, update, view)
+    with Settings.open_session():
+        if user_settings is not None and update.effective_message is not None and update.effective_message.text is not None:
+            user_settings.timezone = update.effective_message.text
+            user_settings.update()
+
+            message = f"Your timezone has been set to: *{user_settings.timezone}* "
+            view = settings_view(message)
+
+            await send_message_view(context, update, view)
 
         return ConversationHandler.END
