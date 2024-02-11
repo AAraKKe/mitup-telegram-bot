6f96182781703e9b350aba28bf13a4e2c218f254
Enrique Araque <minixute@gmail.com>
2024-02-11 16:23:17 +0100

    Delete unused querys.py file

--- mitup_bot/models/querys.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import Session, select
-
-from . import User
-
-
-def get_user_by_tg_user_id(tg_user_id: int) -> User:
-    with Session(engine) as session:
-        statement = select(User).where(User.tg_user_id == tg_user_id)
-        return session.exec(statement)
