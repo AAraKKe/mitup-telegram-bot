c4e64a812758bf6e775be2ca9e6f9aa378e46c5b
Enrique Araque <minixute@gmail.com>
2024-02-13 18:46:59 +0100

    Change import format in the handlers's init

--- mitup_bot/handlers/__init__.py
+++ mitup_bot/handlers/__init__.py
@@ -2,10 +2,13 @@
 # isort: skip_file
 __all__ = ("commands", "HandlersRegistry", "messages", "callback_query", "conversations", "Conversation_Settings_State", "UserExistFilter")
 
-from . import commands
-from . import messages
-from . import callback_query
-from . import conversations
+from . import (
+    commands,
+    messages,
+    callback_query,
+    conversations,
+)
+
 from .personal_filters  import UserExistFilter
 from .conversations_states import Conversation_Settings_State
 from .registry import HandlersRegistry
