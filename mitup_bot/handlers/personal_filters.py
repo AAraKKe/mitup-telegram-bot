2f744c8d275168563607af8a3b3da3f3112f3387
Enrique Araque <minixute@gmail.com>
2024-02-14 09:45:51 +0100

    Remove confused variables

--- mitup_bot/handlers/personal_filters.py
+++ mitup_bot/handlers/personal_filters.py
@@ -7,8 +7,7 @@ from mitup_bot.models import User
 class UserExistFilter(UpdateFilter):
     def filter(self, update: Update) -> bool:
         if update.effective_user is not None:
-            user = update.effective_user.id
             with User.open_session():
-                return User.find_by_tg_user_id(user) is not None
+                return User.find_by_tg_user_id(update.effective_user.id) is not None
 
         return False
