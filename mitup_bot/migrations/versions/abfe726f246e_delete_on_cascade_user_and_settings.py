df2b39a1a6b255b7f3692bbaad92a47e06d1ba77
Enrique Araque <minixute@gmail.com>
2024-02-14 10:17:56 +0100

    Remove unnecesary migration and change the revision id

--- mitup_bot/migrations/versions/abfe726f246e_delete_on_cascade_user_and_settings.py
+++ mitup_bot/migrations/versions/abfe726f246e_delete_on_cascade_user_and_settings.py
@@ -1,7 +1,7 @@
 """Delete on cascade user and settings
 
 Revision ID: abfe726f246e
-Revises: 741e3b4adacf
+Revises: 6ab6bcf55eb7
 Create Date: 2024-01-06 16:30:00.195185+00:00
 
 """
@@ -11,7 +11,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "abfe726f246e"
-down_revision: str | None = "741e3b4adacf"
+down_revision: str | None = "6ab6bcf55eb7"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
