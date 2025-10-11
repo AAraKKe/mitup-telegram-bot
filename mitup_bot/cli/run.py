8a89d39b02bcd9e02d26ef02cfca77885f56ed00
Juanpe Araque <erjuanpea@gmail.com>
2025-10-11 23:04:25 +0000

    🧹 Refactor Dockerfiles and installation process
    See merge request meetupbot/mitup-telegram-bot!163
    
    - Approved by: 
    - Merged by: Juanpe Araque <erjuanpea@gmail.com>
    
    Commits squashed (max 100): 
    
    * 🐛 Ensure the bot is installed in a directory within path
    
    * 🏗️ Improve docker layering
    
    * 🧹 Refactor Dockerfiles and installation process

--- mitup_bot/cli/run.py
+++ mitup_bot/cli/run.py
@@ -1,8 +1,13 @@
 from mitup_bot.cli.cli_commands import MitupCliCommand
 
-if __name__ == "__main__":
+
+def main():
     """This custom command class reads every file under the commands folder and creates
     a command with the name of the file. Each file must contain a method named cli that
     is decorated with a click.command decorator.
     """
     MitupCliCommand(no_args_is_help=True)()
+
+
+if __name__ == "__main__":
+    main()
